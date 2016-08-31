cc6923cc29e1dc6e0cbc7f7d4c0a463a53457d18
Raymond Wang <raymond@blinkingtwelve.net>
2016-08-30 20:28:15 -0500

    fixed improperly configured core.cljs

--- condiments/src/condiments/core.cljs
+++ condiments/src/condiments/core.cljs
@@ -1,2 +1,8 @@
+(ns condiments.core
+  (:require [cljs.nodejs :as nodejs]))
+(nodejs/enable-util-print!) (defn -main [& args]
+      (println "It works!"))
+    (set! *main-cli-fn* -main)
+
 (def xml (js/require "node-xml-lite"))
 (.parseFileSync xml "test.xml")
